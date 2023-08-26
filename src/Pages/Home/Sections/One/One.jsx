208f12a691b086c75f7dd38d764dd28d0372fcb0
Nimul99 <htnimul99@gmail.com>
2023-08-27 04:25:47 +0600

    implement in home page

--- src/Pages/Home/Sections/One/One.jsx
+++ src/Pages/Home/Sections/One/One.jsx
@@ -1,14 +1,33 @@
 import book from "../../../../assets/icons/books.png";
 import OneCompo from "./OneCompo";
+import React, { useEffect, useState } from 'react';
 
 const One = () => {
+  const [courseCount, setCourseCount] = useState(7500);
+  const [instructorCount, setInstructorCount] = useState(15000);
+  const [studentCount, setStudentCount] = useState(750000);
+  const [satisfactionRate, setSatisfactionRate] = useState(110);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setCourseCount(8000);
+      setInstructorCount(1600);
+      setStudentCount(170000);
+      setSatisfactionRate(100);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <div className="bg-accent sm:h-96  py-8 md:py-0  md:h-60">
+    <div className="bg-accent sm:h-96 py-8 md:py-0 md:h-60">
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 sm:pt-16 md:pt-20">
-        <OneCompo image={book} number={"7500+"} text={"Course"} />
-        <OneCompo image={book} number={"1500+"} text={"INSTRUCTORS"} />
-        <OneCompo image={book} number={"165000+"} text={"STUDENTS"} />
-        <OneCompo image={book} number={"100%"} text={"SATISFACTION"} />
+        <OneCompo id="courseCounter" image={book} number={courseCount} text={"Course"} />
+        <OneCompo id="instructorCounter" image={book} number={instructorCount} text={"INSTRUCTORS"} />
+        <OneCompo id="studentCounter" image={book} number={studentCount} text={"STUDENTS"} />
+        <OneCompo id="satisfactionCounter" image={book} number={satisfactionRate} text={"SATISFACTION"} />
       </div>
     </div>
   );
--- src/Pages/Home/Sections/One/OneCompo.jsx
+++ src/Pages/Home/Sections/One/OneCompo.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
+import Odometer from 'react-odometerjs';
+import 'odometer/themes/odometer-theme-minimal.css';
+
 // eslint-disable-next-line react/prop-types
-const OneCompo = ({ image, number, text }) => {
+const OneCompo = ({ id, image, number, text }) => {
   return (
-    <div className="flex flex-col md:flex-row lg:flex-row md:gap-4 items-center justify-center">
+    <div id={id} className="flex flex-col md:flex-row lg:flex-row md:gap-4 items-center justify-center">
       <div className="w-20 h-20 mb-2">
         <img src={image} alt="" />
       </div>
       <div>
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">{number}</h1>
+        <Odometer className="text-2xl md:text-3xl lg:text-4xl font-bold" value={number} />
         <p className="text-base md:text-lg lg:text-xl">{text}</p>
       </div>
     </div>
